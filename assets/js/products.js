b17190547b9b81d836805a8fcb084a6bbc55d698
obinx2000 <halintar99@gmail.com>
2026-06-05 06:01:08 +0700

    tambah indomie gepek rawon soto lamongan

---
+++ assets/js/products.js
@@ -0,0 +1,85 @@
+const products = [
+    {
+        kode: "BR001",
+        nama: "Beras Kepompong 5kg",
+        kategori: "Sembako",
+        harga: 69000,
+        gambar: "assets/images/products/beras-kepompong-5kg.png",
+        stok: "Tersedia",
+        promo: true
+    },
+    {
+        kode: "MG002",
+        nama: "Bimoli 2 Liter",
+        kategori: "Sembako",
+        harga: 35000,
+        gambar: "assets/images/products/minyak-bimoli-2liter.jpg",
+        stok: "Tersedia",
+        promo: true
+    },
+    {
+        kode: "IM003",
+        nama: "Indomie Goreng Original",
+        kategori: "Makanan Instan",
+        harga: 3500,
+        gambar: "assets/images/products/mie-indomie-goreng-original-85gr.jpg",
+        stok: "Tersedia",
+        promo: false
+    },
+    {
+        kode: "IM004",
+        nama: "Indomie Soto 70gr",
+        kategori: "Makanan Instan",
+        harga: 3500,
+        gambar: "assets/images/products/mie-indomie-soto-70gr.png",
+        stok: "Tersedia",
+        promo: false
+    },
+    {
+        kode: "IM005",
+        nama: "mie-indomie-rawon-70gr",
+        kategori: "Makanan Instan",
+        harga: 3500,
+        gambar: "assets/images/products/mie-indomie-rawon-70gr.png",
+        stok: "Tersedia",
+        promo: false
+    },
+    {
+        kode: "IM006",
+        nama: "indomie geprek 70gr",
+        kategori: "Makanan Instan",
+        harga: 3500,
+        gambar: "assets/images/products/mie-indomie-geprek-70gr.png",
+        stok: "Tersedia",
+        promo: false
+    },
+    {
+        kode: "IM006",
+        nama: "Indomie Soto Lamongan 70gr",
+        kategori: "Makanan Instan",
+        harga: 3500,
+        gambar: "assets/images/products/mie-indomie-soto-lamongan-70gr.png",
+        stok: "Tersedia",
+        promo: false
+    },
+
+
+    {
+        kode: "GL004",
+        nama: "Gula KTM 1kg",
+        kategori: "Sembako",
+        harga: 17000,
+        gambar: "assets/images/products/gula-ktm-1kg.png",
+        stok: "Tersedia",
+        promo: false
+    },
+    {
+        kode: "MN001",
+        nama: "Teh Pucuk 250 ml",
+        kategori: "Minuman",
+        harga: 5000,
+        gambar: "assets/images/products/teh-pucuk-350ml.jpg",
+        stok: "Tersedia",
+        promo: true
+    }
+];
